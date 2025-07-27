6dde07e10d3757c2022aa922635013ab248ff6a0
ahmedragab15 <ragabahmed154@gmail.com>
2025-07-27 15:08:12 +0300

    message section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
+"use client";
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/sections/HeroSection";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
+import { ScrollTrigger, ScrollSmoother } from "gsap/all";
+import { useGSAP } from "@gsap/react";
+import MessageSection from "@/sections/MessageSection";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 export default function Home() {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  });
   return (
     <main>
       <Navbar />
-      <HeroSection />
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <HeroSection />
+          <MessageSection/>
+        </div>
+      </div>
     </main>
   );
 }
